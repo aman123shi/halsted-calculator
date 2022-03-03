f79e336bfcae9fe8ef8d5195d2ed6f1f3910ff8c
Amanuel Shiferaw <123aman64@gmail.com>
2022-03-02 22:53:19 -0800

    Revert "testing nav"
    This reverts commit 5751530dec38b97c7d6d58069a122b574f08bf02.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { Routes,RouterModule } from "@angular/router";
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [
-  { path: '/', component: AppComponent },
-  { path: '', component: AppComponent },
-  { path: 'aboutus', component: AboutusComponent },
-  { path: '**', component: AppComponent },
-];
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutusComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    NgxSpinnerModule,
-    RouterModule.forRoot(routes)
+    NgxSpinnerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
