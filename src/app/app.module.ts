a3d93e95f7c90309423e973c814d161346eb017f
Amanuel Shiferaw <123aman64@gmail.com>
2022-03-02 22:44:56 -0800

    cors dependancy added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
+import { Routes,RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-
+const routes: Routes = [
+  { path: '/', component: AppComponent },
+  { path: '', component: AppComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: '**', component: AppComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    NgxSpinnerModule
-    
+    NgxSpinnerModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/helper/datasource.ts
+++ src/app/helper/datasource.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { hostname } from 'os';
+
 
 
 @Injectable({providedIn:'root'})
